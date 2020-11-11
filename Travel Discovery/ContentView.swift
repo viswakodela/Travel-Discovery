44b7a97f288bd934150fdac9a2d8be80b7d8f09a
Viswa Kodela <viswakodela@carfax.com>
2020-11-10 19:41:07 -0500

    Initial Commit

---
+++ Travel Discovery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Travel Discovery
+//
+//  Created by Viswa Kodela on 2020-11-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Travel Discovery/Travel_DiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_DiscoveryApp.swift
+//  Travel Discovery
+//
+//  Created by Viswa Kodela on 2020-11-10.
+//
+
+import SwiftUI
+
+@main
+struct Travel_DiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
